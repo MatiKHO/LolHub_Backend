d7543d330053e5fa494a106d5a203b7c5d73bebe
Matías Gómez Suárez <magosudev@MacBook-Air-de-Matias.local>
2025-04-05 00:42:54 +0200

    CORS fixed

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ const httpServer = createServer(app);
 initializeSocket(httpServer);
 
 app.use(cors({
-  origin: "https://67f05cc7761fb100081e3065--lolhubofficial.netlify.app",
+  origin: "https://main--lolhubofficial.netlify.app",
   credentials: true,
 }));
 app.use(morgan("dev"));
